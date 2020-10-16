bde6b859f6a69e31dd4835f5648abdd75555051c
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-16 14:21:05 -0400

    Commit #9

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,12 +2,20 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
   validates :item_id, :user_id, :title, :rating, presence: true
-  validates :user_id, uniqueness: true
+  validates :user_id_user_id, uniqueness: true
 
-def description?
-  if self.description != nil && self.description != ""
-    true
-  else
-    false
+  attr_accessor :item_id_user_id
+
+  def item_id_user_id
+    "#{self.item_id} #{self.user_id}"
   end
+
+  def description?
+    if self.description != nil && self.description != ""
+      true
+    else
+      false
+    end
+  end
+
 end
