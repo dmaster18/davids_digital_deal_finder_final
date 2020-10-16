852b0ea4470c627f23b613788d53d401dbd3178e
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-16 15:00:27 -0400

    Commit #13

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,10 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
   validates :item_id, :user_id, :title, :rating, presence: true
-  validates :item_and_user_id, uniqueness: true #Prevents more than 1 review by same user
+  validates_uniqueness_of :item_and_user_id #Prevents more than 1 review by same user
 
   def item_and_user_id
-    "#{self.item_id} #{self.user_id}"
+    "#{self.item_id}_#{self.user_id}".to_sym
   end
 
   def description?
